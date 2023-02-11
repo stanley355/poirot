bcf6ed597203bda7cd7ec957c37cca67e1f164b9
Stanley Winata <winatastanley355@gmail.com>
2023-02-11 15:58:45 +0700

    Remove unused trait

---
+++ src/statistics/absolute.rs
@@ -0,0 +1,3 @@
+use super::traits;
+
+// pub fn find_max<T: traits::StaticNumber> ()
--- src/statistics/mod.rs
+++ src/statistics/mod.rs
@@ -1,22 +1,22 @@
 use self::traits::StaticNumber;
 
 pub mod mean;
-pub mod traits;
 pub mod tests;
+pub mod traits;
 
 pub struct Statistics;
 
 impl Statistics {
     pub fn calc_integer_mean<T: StaticNumber>(numbers: &Vec<T>) -> T
     where
-        T: traits::StaticNumber + From<i32>,
+        T: From<i32>,
     {
         mean::calc_integer_mean(numbers)
     }
 
     pub fn calc_float_mean<T: StaticNumber>(numbers: &Vec<T>) -> T
     where
-        T: traits::StaticNumber + From<f32>,
+        T: From<f32>,
     {
         mean::calc_float_mean(numbers)
     }
